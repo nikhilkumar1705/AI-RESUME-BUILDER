34624946fbc1ca21fff098a61fb18ac2dd9def78
nikhilkumarrrr <nikhilkumar80582@gmail.com>
2026-07-18 16:40:05 +0530

    Fix CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,13 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: process.env.BASE_URL,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
